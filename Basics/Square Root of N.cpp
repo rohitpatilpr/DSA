201532b920350f4f4e581c41c7f92c1755231ed2
Rohit Patil <67288441+rohitpatilpr@users.noreply.github.com>
2023-08-27 16:26:13 +0530

    Create Square Root of N.cpp

---
+++ Basics/Square Root of N.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter a Number-"<<endl;
+    cin>>n;
+    
+    cout<<"Sqaure Root-"<<round(sqrt(n));
+}
