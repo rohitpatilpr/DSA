75d85391fd76962ea87f2d8dcfd9e7444e71c556
Rohit Patil <67288441+rohitpatilpr@users.noreply.github.com>
2023-08-27 15:58:22 +0530

    Create Power of a Number.cpp

---
+++ Basics/Power of a Number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n,x;
+    cout<<"Enter a Number & its exponential-"<<endl;
+    cin>>n>>x;
+    
+   cout<<round(pow(n,x));
+}
