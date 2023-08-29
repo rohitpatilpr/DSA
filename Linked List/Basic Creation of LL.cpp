d2c9c628c4ed09c3c757156967c19b6fb29468bb
Rohit Patil <67288441+rohitpatilpr@users.noreply.github.com>
2023-08-29 18:18:48 +0530

    Create Basic Creation of LL.cpp

---
+++ Linked List/Basic Creation of LL.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+    
+    Node(int data){
+        this->data=data;
+        this->next;
+    }
+};
+
+int main() {
+    Node n1(10);
+    Node n2(20);
+    
+    Node *head=&n1;
+    n1.next=&n2;
+    
+    cout<<head->data<<endl;
+    cout<<head->next->data;
+}
