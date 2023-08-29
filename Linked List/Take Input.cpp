47cc5dc588d5f0b7908ad22e39bb347e9737962b
Rohit Patil <67288441+rohitpatilpr@users.noreply.github.com>
2023-08-29 19:01:45 +0530

    Create Take Input.cpp

---
+++ Linked List/Take Input.cpp
@@ -0,0 +1,51 @@
+ #include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+    
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+void printLL(Node *head){
+        Node *temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+            
+        }
+}
+        
+Node *inputLL(){
+    int data;
+    cin>>data;
+    
+    Node *head=NULL;
+    Node *tail=NULL;
+    
+    while(data!=-1){
+        Node *NewNode=new Node(data);
+        if(head==NULL){
+            head=NewNode;
+            tail=NewNode;
+        }
+        else{
+            tail->next=NewNode;
+            tail=tail->next;
+        }
+        cin>>data;
+        
+    }
+    return head;
+}        
+
+    
+int main() {
+    Node *head=inputLL();
+    printLL(head);
+}
