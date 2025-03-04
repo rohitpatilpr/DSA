964d9613f1961f3f5ef8460ab417190967819043
Rohit Patil <67288441+rohitpatilpr@users.noreply.github.com>
2025-03-05 02:28:06 +0530

    Update Length of String.cpp

--- Strings/Length of String.cpp
+++ Strings/Length of String.cpp
@@ -1,1 +1,19 @@
-#
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    string s;
+    int count=0;
+    
+    getline(cin,s);
+    
+    int len=s.length();
+    
+    for(int i=0; s[i] != '\0'; i++){
+        count++;
+    }
+    
+    cout<<len<<endl<<count;
+	
+}
